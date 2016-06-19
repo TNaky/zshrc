211dd546a7557728c21e8eb13a2e03766e0932ce
TNak@MacMini <michibata.dev@gmail.com>
2016-06-19 12:50:19 +0900

    zshの設定をするシェルスクリプトを追加

---
+++ install.sh
@@ -0,0 +1,13 @@
+#! /bin/zsh
+if [ ! -d ${HOME}/.zsh ]; then
+  if [ ! -d ${HOME}/.zprezto ]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+  fi
+  git clone https://github.com/TNaky/zshrc.git ${HOME}/.zsh
+  ln -s ${HOME}/.zsh/zshrc ${HOME}/.zshrc
+  ln -s ${HOME}/.zsh/zsh_aliases ${HOME}/.zsh_aliases
+fi
