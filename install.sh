416fd40bc4a5ebd69e163ca4098538648db18bb3
TNak@MacMini <michibata.dev@gmail.com>
2016-06-26 21:34:00 +0900

    エラー部分を修正

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [ ! -d ${HOME}/.zsh ]; then
   if [ ! -d ${HOME}/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
     setopt EXTENDED_GLOB
-    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/z*; do
       ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     done
   fi
