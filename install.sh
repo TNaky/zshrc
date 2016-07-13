26ab009b4ef7a3e459402a3292adf049f363ac41
TNak@MacBookAir <michibata.dev@gmail.com>
2016-07-14 02:15:47 +0900

    不必要なファイルのリンクが作成されないよぅに修正

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 if [ ! -d ${HOME}/.zsh ]; then
   if [ ! -d ${HOME}/.zprezto ]; then
     git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-    setopt EXTENDED_GLOB
     for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/z*; do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+      farray=(`echo $rcfile | tr -s '/' ' '`)
+      rcfname=${farray[${#farray[@]} - 1]}
+      if [ ! ${rcfname} = "zshrc" ]; then
+        ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+        # echo ${rcfname}
+      fi
     done
   fi
   git clone https://github.com/TNaky/zshrc.git ${HOME}/.zsh
